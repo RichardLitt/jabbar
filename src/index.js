42c0d3a1d8a80a70d39558ced2ab2122ca8f06b6
Richard Littauer <richard+github@burntfen.com>
2019-10-21 15:19:30 +0200

    Add code to get stargazers

--- src/index.js
+++ src/index.js
@@ -12,10 +12,51 @@ function timeout (ms) {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-// Get all watchers information from a repository
-const repoQuery = function (owner, repo, endCursor) {
+// Get all star gazer information from a repository
+const stargazersQuery = function (owner, repo, endCursor) {
   return {
-    query: `query getWatchers($owner: String!, $repo: String!, $endCursor: String!) {
+    query: `query stargazers($owner: String!, $repo: String!, $endCursor: String) {
+    repository(name: $repo, owner: $owner) {
+      stargazers(first: 100, after: $endCursor) {
+        totalCount
+        edges {
+          node {
+            name
+            login
+            company
+            bio
+            websiteUrl
+            url
+            organizations(first: 100) {
+              totalCount
+              edges {
+                node {
+                  name
+                  description
+                  websiteUrl
+                }
+              }
+            }
+          }
+        }
+        pageInfo {
+          hasNextPage
+          endCursor
+          startCursor
+        }
+      }
+    }
+  }`,
+    repo: repo,
+    owner: owner,
+    endCursor: endCursor || null
+  }
+}
+
+// Get all watchers information from a repository
+const watchersQuery = function (owner, repo, endCursor) {
+  return {
+    query: `query watchers($owner: String!, $repo: String!, $endCursor: String) {
     repository(name: $repo, owner: $owner) {
       watchers(first: 100, after: $endCursor) {
         totalCount
@@ -49,38 +90,50 @@ const repoQuery = function (owner, repo, endCursor) {
   }`,
     repo: repo,
     owner: owner,
-    endCursor: endCursor || ''
+    endCursor: endCursor || null
   }
 }
 
 // Note: This only works if the object for a 3-deep list of type { obj: field: edge }
-async function paginate3DepthEdge (owner, repo, endCursor, obj, objName, path) {
+async function paginate3DepthEdge (q, owner, repo, endCursor, obj, objName, path) {
   // If there are pages, recursively concatinate res to the resulting object
-  let result = await graphqlt(repoQuery(owner, repo, endCursor))
+  let result = await graphqlt(q(owner, repo, endCursor))
   obj[path].edges = obj[path].edges.concat(result[objName][path].edges)
   obj[path].pageInfo = result[objName][path].pageInfo
 
   if (obj[path].pageInfo.hasNextPage) {
     let endCursor = obj[path].pageInfo.endCursor
     await timeout(1000) // Let's be nice.
-    obj = await paginate3DepthEdge(owner, repo, endCursor, obj, objName, path)
+    obj = await paginate3DepthEdge(q, owner, repo, endCursor, obj, objName, path)
   }
 
   return obj
 }
 
-const callGraphql = async function (owner, repo) {
+// Getting watchers and stargazers are basically the same, hence this wrapper.
+const getRepoInteractorsWrapper = async function (q, owner, repo, interactors) {
   try {
-    let { repository } = await graphqlt(repoQuery(owner, repo))
-    if (repository.watchers.pageInfo.hasNextPage) {
-      let endCursor = repository.watchers.pageInfo.endCursor
-      repository = await paginate3DepthEdge(owner, repo, endCursor, repository, 'repository', 'watchers')
+    let { repository } = await graphqlt(q(owner, repo))
+    if (repository[interactors].pageInfo.hasNextPage) {
+      let endCursor = repository[interactors].pageInfo.endCursor
+      repository = await paginate3DepthEdge(q, owner, repo, endCursor, repository, 'repository', interactors)
     }
-    return repository.watchers.edges
+    return repository[interactors].edges
   } catch (error) {
-    console.log('Request failed:', error.request) // { query, variables: {}, headers: { authorization: 'token secret123' } }
-    console.log(error.message) // Field 'bioHtml' doesn't exist on type 'User'
+    console.log('Request failed:', error.request)
+    console.log(error.message)
   }
 }
 
-module.exports = callGraphql
+const getWatchers = async function (owner, repo) {
+  return getRepoInteractorsWrapper(watchersQuery, owner, repo, 'watchers')
+}
+
+const getStargazers = async function (owner, repo) {
+  return getRepoInteractorsWrapper(stargazersQuery, owner, repo, 'stargazers')
+}
+
+module.exports = {
+  getStargazers,
+  getWatchers
+}
--- test/test.js
+++ test/test.js
@@ -6,20 +6,43 @@ const chaiAsPromised = require('chai-as-promised')
 const { expect } = chai
 chai.use(chaiAsPromised)
 
-describe('When called with correct args', function () {
+describe('When getWatchers is called it', function () {
   this.timeout(5000)
 
   it('should resolve', () => {
-    return j('RichardLitt', 'jabbar')
+    return j.getWatchers('RichardLitt', 'jabbar')
   })
 
-  it('Should should return at least the minimal watcher', async () => { //
-    const data = await j('RichardLitt', 'jabbar')
+  it('should should return at least the minimal watcher', async () => { //
+    const data = await j.getWatchers('RichardLitt', 'jabbar')
     expect(data[0].node.login).to.equal('RichardLitt')
   })
 
-  it('Should paginate', async () => { // no done
-    const data = await j('orbitdb', 'orbit-db')
+  it('should get at least first page of orgs for a watcher', async () => { //
+    const data = await j.getWatchers('RichardLitt', 'jabbar')
+    expect(data[0].node.organizations.totalCount).to.be.above(40)
+  })
+
+  it('should paginate', async () => { // no done
+    const data = await j.getWatchers('orbitdb', 'orbit-db')
+    // TODO There's some issue in pagination. This should be 127, but it returrns 126
+    expect(data.length).to.be.above(100)
+  })
+})
+
+describe.only('When getStargazers is called it', function () {
+  this.timeout(5000)
+  it('should resolve', () => {
+    return j.getStargazers('RichardLitt', 'jabbar')
+  })
+
+  it('should should return at least the minimal stargazer', async () => { //
+    const data = await j.getStargazers('RichardLitt', 'jabbar')
+    expect(data[9].node.login).to.equal('RichardLitt')
+  })
+
+  it('should paginate', async () => { // no done
+    const data = await j.getStargazers('RichardLitt', 'knowledge')
     // TODO There's some issue in pagination. This should be 127, but it returrns 126
     expect(data.length).to.be.above(100)
   })
