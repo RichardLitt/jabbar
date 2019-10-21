f05819f87519bfdc7d861eb1832ae3ea4bd59dd1
Richard Littauer <richard+github@burntfen.com>
2019-10-21 14:26:53 +0200

    Enable pagination and add test #9

--- src/index.js
+++ src/index.js
@@ -1,29 +1,23 @@
-const { graphql } = require("@octokit/graphql")
+const { graphql } = require('@octokit/graphql')
 const token = process.env.JABBAR_TOKEN
 
-graphqlt = graphql.defaults({
+const graphqlt = graphql.defaults({
   headers: {
     authorization: `token ${token}`
   }
 })
 
-
 // Small helper function
-function timeout(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+function timeout (ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-// Small helper function
-function endCursorStr (c) {
-  return `${(c) ? ', after:"' + c + '"' : ''}`
-}
-
-// TODO There might be an easier way of doing variables
 // Get all watchers information from a repository
 const repoQuery = function (owner, repo, endCursor) {
-  return `{
-    repository(name: "${repo}", owner: "${owner}") {
-      watchers(first: 100${endCursorStr(endCursor)}) {
+  return {
+    query: `query getWatchers($owner: String!, $repo: String!, $endCursor: String!) {
+    repository(name: $repo, owner: $owner) {
+      watchers(first: 100, after: $endCursor) {
         totalCount
         edges {
           node {
@@ -52,20 +46,24 @@ const repoQuery = function (owner, repo, endCursor) {
         }
       }
     }
-  }`
+  }`,
+    repo: repo,
+    owner: owner,
+    endCursor: endCursor || ''
+  }
 }
 
 // Note: This only works if the object for a 3-deep list of type { obj: field: edge }
-async function paginate3DepthEdge (query, obj, objName, path) {
+async function paginate3DepthEdge (owner, repo, endCursor, obj, objName, path) {
   // If there are pages, recursively concatinate res to the resulting object
-  let result = await graphqlt(query)
+  let result = await graphqlt(repoQuery(owner, repo, endCursor))
   obj[path].edges = obj[path].edges.concat(result[objName][path].edges)
   obj[path].pageInfo = result[objName][path].pageInfo
 
   if (obj[path].pageInfo.hasNextPage) {
-    endCursor = obj[path].pageInfo.endCursor
-    await timeout(1000)
-    obj = await paginate3DepthEdge(query, obj, objName, path)
+    let endCursor = obj[path].pageInfo.endCursor
+    await timeout(1000) // Let's be nice.
+    obj = await paginate3DepthEdge(owner, repo, endCursor, obj, objName, path)
   }
 
   return obj
@@ -76,14 +74,12 @@ const callGraphql = async function (owner, repo) {
     let { repository } = await graphqlt(repoQuery(owner, repo))
     if (repository.watchers.pageInfo.hasNextPage) {
       let endCursor = repository.watchers.pageInfo.endCursor
-      repository = await paginate3DepthEdge(repoQuery(owner, repo, endCursor), repository, 'repository', 'watchers')
+      repository = await paginate3DepthEdge(owner, repo, endCursor, repository, 'repository', 'watchers')
     }
-    // TODO Print this in a nice way.
-    // console.log(repository.watchers.edges)
     return repository.watchers.edges
   } catch (error) {
-    console.log("Request failed:", error.request); // { query, variables: {}, headers: { authorization: 'token secret123' } }
-    console.log(error.message); // Field 'bioHtml' doesn't exist on type 'User'
+    console.log('Request failed:', error.request) // { query, variables: {}, headers: { authorization: 'token secret123' } }
+    console.log(error.message) // Field 'bioHtml' doesn't exist on type 'User'
   }
 }
 
--- test/test.js
+++ test/test.js
@@ -7,13 +7,20 @@ const { expect } = chai
 chai.use(chaiAsPromised)
 
 describe('When called with correct args', function () {
+  this.timeout(5000)
+
   it('should resolve', () => {
     return j('RichardLitt', 'jabbar')
   })
 
-  it('Should should return at least the minimal watcher', async () => { // no done
-    const login = 'RichardLitt'
-    const data = await j(login, 'jabbar')
-    expect(data[0].node.login).to.equal(login)
+  it('Should should return at least the minimal watcher', async () => { //
+    const data = await j('RichardLitt', 'jabbar')
+    expect(data[0].node.login).to.equal('RichardLitt')
+  })
+
+  it('Should paginate', async () => { // no done
+    const data = await j('orbitdb', 'orbit-db')
+    // TODO There's some issue in pagination. This should be 127, but it returrns 126
+    expect(data.length).to.be.above(100)
   })
 })
