fdb5ae651f50b535c80612fc4ea07ffea9e665fd
Richard Littauer <richard+github@burntfen.com>
2019-11-04 10:04:01 -0500

    refactor: clean up cruft

--- src/index.js
+++ src/index.js
@@ -28,9 +28,83 @@ function clean (arr) {
   })
 }
 
-// Get all star gazer information from a repository
-const stargazersQuery = function (owner, repo, endCursor) {
-  return {
+// Note: This only works if the object for a 3-deep list of type { obj: field: edge }
+async function paginate3DepthEdge (query, obj, path) {
+  // If there are pages, recursively concatinate res to the resulting object
+  let result = await graphqlt(query)
+  obj[path].edges = obj[path].edges.concat(result['repository'][path].edges)
+  obj[path].pageInfo = result['repository'][path].pageInfo
+
+  if (obj[path].pageInfo.hasNextPage) {
+    query.endCursor = obj[path].pageInfo.endCursor
+    await timeout(1000) // Let's be nice.
+    obj = await paginate3DepthEdge(query, obj, path)
+  }
+
+  return obj
+}
+
+// Getting watchers and stargazers are basically the same, hence this wrapper.
+const getRepoInteractorsWrapper = async function (query, interactors) {
+  try {
+    let { repository } = await graphqlt(query)
+    if (repository[interactors].pageInfo.hasNextPage) {
+      query.endCursor = repository[interactors].pageInfo.endCursor
+      repository = await paginate3DepthEdge(query, repository, interactors)
+    }
+    return repository[interactors].edges
+  } catch (error) {
+    console.log('Request failed:', error.request)
+    console.log(error.message)
+  }
+}
+
+const getWatchers = async function (owner, repo) {
+  let endCursor = null
+  let query = {
+    query: `query watchers($owner: String!, $repo: String!, $endCursor: String) {
+    repository(name: $repo, owner: $owner) {
+      watchers(first: 100, after: $endCursor) {
+        totalCount
+        edges {
+          node {
+            name
+            login
+            company
+            bio
+            websiteUrl
+            url
+            organizations(first: 100) {
+              totalCount
+              edges {
+                node {
+                  name
+                  login
+                  description
+                  websiteUrl
+                }
+              }
+            }
+          }
+        }
+        pageInfo {
+          hasNextPage
+          endCursor
+          startCursor
+        }
+      }
+    }
+  }`,
+    repo: repo,
+    owner: owner,
+    endCursor: endCursor || null
+  }
+  return clean(await getRepoInteractorsWrapper(query, 'watchers'))
+}
+
+const getStargazers = async function (owner, repo) {
+  let endCursor = null
+  let query = {
     query: `query stargazers($owner: String!, $repo: String!, $endCursor: String) {
     repository(name: $repo, owner: $owner) {
       stargazers(first: 100, after: $endCursor) {
@@ -69,87 +143,7 @@ const stargazersQuery = function (owner, repo, endCursor) {
     owner: owner,
     endCursor: endCursor || null
   }
-}
-
-// Get all watchers information from a repository
-const watchersQuery = function (owner, repo, endCursor) {
-  return {
-    query: `query watchers($owner: String!, $repo: String!, $endCursor: String) {
-    repository(name: $repo, owner: $owner) {
-      watchers(first: 100, after: $endCursor) {
-        totalCount
-        edges {
-          node {
-            name
-            login
-            company
-            bio
-            websiteUrl
-            url
-            organizations(first: 100) {
-              totalCount
-              edges {
-                node {
-                  name
-                  login
-                  description
-                  websiteUrl
-                }
-              }
-            }
-          }
-        }
-        pageInfo {
-          hasNextPage
-          endCursor
-          startCursor
-        }
-      }
-    }
-  }`,
-    repo: repo,
-    owner: owner,
-    endCursor: endCursor || null
-  }
-}
-
-// Note: This only works if the object for a 3-deep list of type { obj: field: edge }
-async function paginate3DepthEdge (q, owner, repo, endCursor, obj, objName, path) {
-  // If there are pages, recursively concatinate res to the resulting object
-  let result = await graphqlt(q(owner, repo, endCursor))
-  obj[path].edges = obj[path].edges.concat(result[objName][path].edges)
-  obj[path].pageInfo = result[objName][path].pageInfo
-
-  if (obj[path].pageInfo.hasNextPage) {
-    let endCursor = obj[path].pageInfo.endCursor
-    await timeout(1000) // Let's be nice.
-    obj = await paginate3DepthEdge(q, owner, repo, endCursor, obj, objName, path)
-  }
-
-  return obj
-}
-
-// Getting watchers and stargazers are basically the same, hence this wrapper.
-const getRepoInteractorsWrapper = async function (q, owner, repo, interactors) {
-  try {
-    let { repository } = await graphqlt(q(owner, repo))
-    if (repository[interactors].pageInfo.hasNextPage) {
-      let endCursor = repository[interactors].pageInfo.endCursor
-      repository = await paginate3DepthEdge(q, owner, repo, endCursor, repository, 'repository', interactors)
-    }
-    return repository[interactors].edges
-  } catch (error) {
-    console.log('Request failed:', error.request)
-    console.log(error.message)
-  }
-}
-
-const getWatchers = async function (owner, repo) {
-  return clean(await getRepoInteractorsWrapper(watchersQuery, owner, repo, 'watchers'))
-}
-
-const getStargazers = async function (owner, repo) {
-  return clean(await getRepoInteractorsWrapper(stargazersQuery, owner, repo, 'stargazers'))
+  return clean(await getRepoInteractorsWrapper(query, 'stargazers'))
 }
 
 const getAllUsers = async function (owner, repo) {
